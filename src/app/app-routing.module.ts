35a2735b4bad7cdd1da565dbe7d8cbf7b7c92293
Moshe Wainner <Moshewiner13@gmail.com>
2020-03-16 19:30:23 +0200

    Added basic home page without any logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { WhatsappComponent } from './pages/whatsapp/whatsapp.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'whatsapp',
+    component: WhatsappComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { WhatsappComponent } from './pages/whatsapp/whatsapp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    WhatsappComponent
   ],
   imports: [
     BrowserModule,
